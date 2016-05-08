0b0efc3cfe2886d2ee7352244594987157e56bd2
Clement Escoffier <clement.escoffier@gmail.com>
2016-05-08 16:47:04 +0200

    Improve discovery bridge documentation

--- vertx-discovery-service/src/main/java/io/vertx/ext/discovery/package-info.java
+++ vertx-discovery-service/src/main/java/io/vertx/ext/discovery/package-info.java
@@ -551,6 +551,18 @@
  * You can provide your own bridge by implementing the {@link io.vertx.ext.discovery.spi.DiscoveryBridge} interface and
  * register it using
  * {@link io.vertx.ext.discovery.DiscoveryService#registerDiscoveryBridge(DiscoveryBridge, io.vertx.core.json.JsonObject)}.
+ *
+ * The second parameter can provide an optional configuration for the bridge.
+ *
+ * When the bridge is registered the
+ * {@link io.vertx.ext.discovery.spi.DiscoveryBridge#start(io.vertx.core.Vertx, io.vertx.ext.discovery.DiscoveryService, io.vertx.core.json.JsonObject, io.vertx.core.Handler)}
+ * method is called. It lets you configure the bridge. When the bridge is configured, ready and has imported /
+ * exported the initial services, it must calls the given {@link io.vertx.core.Handler}. Notice that, as bridge may
+ * block, the `start` method is called in a `worker` thread.
+ *
+ * When the discovery service is stopped, the bridge is stopped. The
+ * {@link io.vertx.ext.discovery.spi.DiscoveryBridge#stop(io.vertx.core.Vertx, io.vertx.ext.discovery.DiscoveryService)}
+ * method is called that provides the opportunity to cleanup resources, removed imported / exported services...
  * 
  * Notice than in a cluster, only one member needs to register the bridge as the records are accessible by all members.
  */
