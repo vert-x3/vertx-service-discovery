53b5d33c095fddb54c735dde34ba13ee86adc384
Cafeinoman <cafeinoman@openaliasbox.org>
2017-04-23 08:33:21 +0000

    Change the test to avoid using the consul-client tests

--- vertx-service-discovery-backend-consul/src/test/java/io/vertx/servicediscovery/backend/consul/ConsulBackendTest.java
+++ vertx-service-discovery-backend-consul/src/test/java/io/vertx/servicediscovery/backend/consul/ConsulBackendTest.java
@@ -1,54 +1,60 @@
 package io.vertx.servicediscovery.backend.consul;
 
+import fr.javacrea.vertx.consul.test.utils.ConsulTestEnv;
 import io.vertx.core.Vertx;
-import io.vertx.ext.consul.ConsulClient;
-import io.vertx.ext.consul.ConsulContext;
-import io.vertx.ext.consul.ConsulTestSuite;
-import io.vertx.ext.consul.impl.ConsulClientImpl;
+import io.vertx.ext.consul.ConsulClientOptions;
 import io.vertx.servicediscovery.spi.ServiceDiscoveryBackend;
 import io.vertx.servicediscovery.spi.ServiceDiscoveryBackendTest;
-import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 
-import java.util.function.Function;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import static com.jayway.awaitility.Awaitility.await;
+import static org.hamcrest.core.Is.is;
 
 /**
  * @author <a href="mailto:cafeinoman@openaliasbox.org>Francois Delalleau</a>
  */
 public class ConsulBackendTest extends ServiceDiscoveryBackendTest {
 
-  private static Function<Vertx, ConsulContext> ctxFactory;
-  private ConsulContext ctx;
+  private static ConsulTestEnv env;
+  private static ConsulClientOptions options;
 
   @BeforeClass
   static public void startConsul() throws Exception {
-    ctxFactory = vertx -> new ConsulContext(
-      opts -> new ConsulClientImpl(vertx, opts),
-      ConsulClient::close
-    );
+    AtomicBoolean done = new AtomicBoolean(false);
+    env = ConsulTestEnv.create(Vertx.vertx());
+    env.startOne(null, true, null, null).setHandler(ar -> {
+      if (ar.succeeded()) {
+        env.clientOptionsForNode(ar.result(), null).setHandler(opt -> {
+          if (opt.succeeded()) {
+            options = opt.result();
+            done.set(true);
+          } else {
+            opt.cause().printStackTrace();
+            throw new RuntimeException(opt.cause());
+          }
+        });
+      } else {
+        throw new RuntimeException(ar.cause());
+      }
+    });
+    await().atMost(4, TimeUnit.MINUTES).untilAtomic(done, is(true)); // download of the binaries can take long time
   }
 
   @AfterClass
   static public void stopConsul() {
-    ConsulTestSuite.stopConsul();
-  }
-
-
-  @After
-  @Override
-  public void tearDown() {
-    ctx.stop();
-    ctx = null;
-    super.tearDown();
+    AtomicBoolean done = new AtomicBoolean(false);
+    env.close(n -> done.set(true));
+    await().atMost(4, TimeUnit.MINUTES).untilAtomic(done, is(true));
   }
 
   @Override
   protected ServiceDiscoveryBackend createBackend() {
-    ctx = ctxFactory.apply(vertx);
-    ctx.start();
     ConsulBackendService backend = new ConsulBackendService();
-    backend.init(vertx, ctx.writeClientOptions().toJson());
+    backend.init(vertx, options.toJson());
     return backend;
   }
 }
